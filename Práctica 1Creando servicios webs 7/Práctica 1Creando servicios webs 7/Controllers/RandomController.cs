cc0d9d8722e3cab85e0bf8b033d88c5d6fc8990f
RonaldJav-P <ronaldjavierperez8@gmail.com>
2025-06-12 17:38:56 -0400

    Ejercicio 7 Terminado

--- Práctica 1Creando servicios webs 7/Práctica 1Creando servicios webs 7/Controllers/RandomController.cs
+++ Práctica 1Creando servicios webs 7/Práctica 1Creando servicios webs 7/Controllers/RandomController.cs
@@ -1,6 +1,26 @@
-﻿namespace Práctica_1Creando_servicios_webs_7.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Práctica_1Creando_servicios_webs_7.Controllers
 {
-    public class RandomController
+    public class RandomController : ControllerBase
     {
+        [HttpGet("Random")]
+        public IActionResult GetRandomNumber([FromQuery] int min, [FromQuery] int max)
+        {
+            if (min > max)
+            {
+                return BadRequest(new { error = "El valor mínimo no puede ser mayor que el valor máximo." });
+            }
+
+            var random = new Random();
+            int result = random.Next(min, max + 1);
+
+            return Ok(new
+            {
+                min,
+                max,
+                randomNumber = result
+            });
+        }
     }
 }
