5afec817a0e6cba2f4bf7ced37f531a34337b011
RonaldJav-P <ronaldjavierperez8@gmail.com>
2025-06-12 15:18:13 -0400

    Ejercicio 2 Terminado

--- Práctica 1Creando servicios webs 2/Práctica 1Creando servicios webs 2/Controllers/CalculatorController.cs
+++ Práctica 1Creando servicios webs 2/Práctica 1Creando servicios webs 2/Controllers/CalculatorController.cs
@@ -1,6 +1,49 @@
-﻿namespace Práctica_1Creando_servicios_webs_2.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Práctica_1Creando_servicios_webs_2.Controllers
 {
-    public class CalculatorController
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class CalculatorController : ControllerBase
     {
+        [HttpGet("add")]
+
+        public IActionResult Add([FromQuery] double c, [FromQuery] double d)
+        {
+            var result = c + d;
+            return Ok(new { result });
+
+        }
+
+        [HttpGet("subtract")]
+
+        public IActionResult Subtract([FromQuery] double c, [FromQuery] double d)
+        {
+            var result = c - d;
+            return Ok(new { result });
+        }
+
+
+        [HttpGet("multiply")]
+        public IActionResult Multiply([FromQuery] double c, [FromQuery] double d)
+        {
+            var result = c * d;
+            return Ok(new { result });
+        }
+
+
+        [HttpGet("divide")]
+        public IActionResult Divide([FromQuery] double c, [FromQuery] double d)
+        {
+            if (d == 0)
+            {
+                return BadRequest(new { error = "No es posible dividir entre cero." });
+            }
+
+            var result = c / d;
+            return Ok(new { result });
+        }
     }
 }
