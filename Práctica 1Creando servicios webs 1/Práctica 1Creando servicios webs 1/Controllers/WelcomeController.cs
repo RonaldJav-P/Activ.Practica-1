dc9a32a20bcbf123f06b4eaacd003e6f2a8346dc
RonaldJav-P <ronaldjavierperez8@gmail.com>
2025-06-12 14:54:53 -0400

    Ejercicio 1 Terminado

--- Práctica 1Creando servicios webs 1/Práctica 1Creando servicios webs 1/Controllers/WelcomeController.cs
+++ Práctica 1Creando servicios webs 1/Práctica 1Creando servicios webs 1/Controllers/WelcomeController.cs
@@ -1,6 +1,26 @@
-﻿namespace Práctica_1Creando_servicios_webs_1.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Práctica_1Creando_servicios_webs_1.Controllers
 {
-    public class WelcomeController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WelcomeController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetWelcomeMessage([FromQuery] string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return BadRequest(new { message = "El parámetro 'name' es obligatorio." });
+            }
+
+            var response = new
+            {
+                message = $"¡Bienvenido, {name}!"
+            };
+
+            return Ok(response);
+        }
     }
 }
