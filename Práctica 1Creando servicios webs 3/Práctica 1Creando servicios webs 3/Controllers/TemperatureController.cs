ce5327daac68b27b00b9daa0a17baf7b71f41920
RonaldJav-P <ronaldjavierperez8@gmail.com>
2025-06-12 16:42:09 -0400

    Ejercicio 3 Terminado

--- Práctica 1Creando servicios webs 3/Práctica 1Creando servicios webs 3/Controllers/TemperatureController.cs
+++ Práctica 1Creando servicios webs 3/Práctica 1Creando servicios webs 3/Controllers/TemperatureController.cs
@@ -1,6 +1,25 @@
-﻿namespace Práctica_1Creando_servicios_webs_3.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Práctica_1Creando_servicios_webs_3.Controllers
 {
-    public class TemperatureController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TemperatureController : ControllerBase
     {
+
+        [HttpGet("toFahrenheit")]
+        public IActionResult ToFahrenheit([FromQuery] double celsius)
+        {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            return Ok(new { celsius, fahrenheit });
+        }
+
+        [HttpGet("toCelsius")]
+        public IActionResult ToCelsius([FromQuery] double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            return Ok(new { fahrenheit, celsius });
+        }
     }
 }
